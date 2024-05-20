94008c2896526242e04d426a7620f3ad6c00feb7
Endy-Silva <endyfsilvaa@gmail.com>
2024-05-20 19:58:21 -0300

    inicio

---
+++ projeto-html,css,Jscript/contato/contato.js
@@ -0,0 +1,8 @@
+// script.js
+function externalFunction() {
+    alert('Função externa chamada!');
+    // Adicione aqui o código que você quer executar
+    function goHome() {
+        window.location.href = 'projeto.html';
+    }
+}
